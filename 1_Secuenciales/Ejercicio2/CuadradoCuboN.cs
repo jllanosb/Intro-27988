092936900fb8fbaf6abf74244a11c37aada22b61
Jaime Llanos <jllanosb@unc.edu.pe>
2026-05-21 08:59:59 -0500

    Ejercicio 2

---
+++ 1_Secuenciales/Ejercicio2/CuadradoCuboN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class CuadradoCuboN
+    {
+        static void Main(string[] args)
+        {
+            int n, cuadrado, cubo;
+            Console.WriteLine("Ingrese un numero");
+            n=int.Parse(Console.ReadLine());
+            cuadrado = n * n;
+            cubo = n * n * n;
+            Console.WriteLine($"El cuadrado de {n} es : {cuadrado}");
+            Console.WriteLine($"El cubo de {n} es: {cubo}");
+        }
+    }
+}
